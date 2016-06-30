78226ed99e5d23d2c6ae5fb534f7bd66351efbfd
Patrick Hund <pahund@team.mobile.de>
2016-06-30 19:30:43 +0200

    adjusted quotes and indentation
    Change-Id: I95a9476256143bae6a1d34cf295710985f853fe4

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,10 +1,10 @@
-import { combineReducers } from 'redux';
-import { routerReducer as routing } from 'react-router-redux';
-import counter from './counter';
+import { combineReducers } from "redux";
+import { routerReducer as routing } from "react-router-redux";
+import counter from "./counter";
 
 const rootReducer = combineReducers({
-  counter,
-  routing
+    counter,
+    routing
 });
 
 export default rootReducer;
--- webpack.config.electron.js
+++ webpack.config.electron.js
@@ -1,46 +1,46 @@
-import webpack from 'webpack';
-import baseConfig from './webpack.config.base';
+import webpack from "webpack";
+import baseConfig from "./webpack.config.base";
 
 export default {
-  ...baseConfig,
+    ...baseConfig,
 
-  devtool: 'source-map',
+    devtool: "source-map",
 
-  entry: ['babel-polyfill', './main.development'],
+    entry: ["babel-polyfill", "./main.development"],
 
-  output: {
-    ...baseConfig.output,
-    path: __dirname,
-    filename: './main.js'
-  },
+    output: {
+        ...baseConfig.output,
+        path: __dirname,
+        filename: "./main.js"
+    },
 
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false
-      }
-    }),
-    new webpack.BannerPlugin(
-      'require("source-map-support").install();',
-      { raw: true, entryOnly: false }
-    ),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
-    })
-  ],
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false
+            }
+        }),
+        new webpack.BannerPlugin(
+            "require(\"source-map-support\").install();",
+            { raw: true, entryOnly: false }
+        ),
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: JSON.stringify("production")
+            }
+        })
+    ],
 
-  target: 'electron-main',
+    target: "electron-main",
 
-  node: {
-    __dirname: false,
-    __filename: false
-  },
+    node: {
+        __dirname: false,
+        __filename: false
+    },
 
-  externals: [
-    ...baseConfig.externals,
-    'font-awesome',
-    'source-map-support'
-  ]
+    externals: [
+        ...baseConfig.externals,
+        "font-awesome",
+        "source-map-support"
+    ]
 };

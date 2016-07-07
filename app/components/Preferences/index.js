ae991f1ccfc920867f42af5bfdc53e3f5d4595ea
Patrick Hund <pahund@team.mobile.de>
2016-07-07 21:40:20 +0200

    preferences midi output selection
    Change-Id: Ic3e24c07ba2c09fa1a71e0570cd49516316955fb

--- app/components/Preferences/index.js
+++ app/components/Preferences/index.js
@@ -8,18 +8,38 @@ import React, { PropTypes } from "react";
 import styles from "./styles.css";
 import classnames from "classnames";
 
-const Preferences = ({ close }) => (
+const Preferences = ({ close, midi: { outputs, selectedOutput } }) => (
     <div>
         <h1>
             <span>Preferences</span>
-            <i className={classnames("fa fa-close", styles.buttonTopRight)}
+            <i className={classnames("fa fa-close", styles.button, styles.buttonTopRight)}
                onClick={close} />
         </h1>
+        <div className={styles.prefRow}>
+            <label htmlFor="midiOutput">MIDI output bus</label>
+            <div className={styles.fieldWithButton}>
+                <select id="midiOutput" value={selectedOutput}>
+                    {
+                        Array.from(outputs.values()).map(output => (
+                            <option key={output.id}
+                                    value={output.id}>
+                                {output.name}
+                            </option>
+                        ))
+                    }
+                </select>
+                <i className={classnames("fa fa-refresh", styles.button)}></i>
+            </div>
+        </div>
     </div>
 );
 
 Preferences.propTypes = {
-    close: PropTypes.func.isRequired
+    close: PropTypes.func.isRequired,
+    midi: PropTypes.shape({
+        outputs: PropTypes.object.isRequired,
+        selectedOutput: PropTypes.string.isRequired
+    }).isRequired
 };
 
 export default Preferences;
